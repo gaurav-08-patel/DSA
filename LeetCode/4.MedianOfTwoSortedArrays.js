7bfa6207a7ea17ef45f33f6213506cebacd2f5cd
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-02-01 21:33:36 +0530

    solved #4

---
+++ LeetCode/4.MedianOfTwoSortedArrays.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+    let mergedArr = [...nums1, ...nums2];
+    let sortedArr = mergedArr.sort((a, b) => a - b);
+
+    if (sortedArr.length % 2 === 0) {
+        let median =
+            (sortedArr[sortedArr.length / 2 - 1] +
+                sortedArr[sortedArr.length / 2]) /
+            2;
+        return median;
+    } else {
+        return sortedArr[(sortedArr.length - 1) / 2];
+    }
+};
