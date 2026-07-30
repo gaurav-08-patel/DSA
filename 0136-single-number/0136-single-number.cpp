8120b4e869dcd9590d4416b4f02c0599733e38f0
Gaurav Patel <167123465+gaurav-08-patel@users.noreply.github.com>
2026-07-30 23:30:13 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.36%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xorVal = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            xorVal ^= nums[i];
+        }
+
+        return xorVal;
+    }
+};
