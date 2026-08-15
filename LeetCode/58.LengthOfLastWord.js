1d0dc4115c8d661e3a19207192caf4de8c57452b
Gaurav Patel <gauravpatelrounak@gmail.com>
2026-08-15 13:51:12 +0530

    Delete LeetCode/58.LengthOfLastWord.js

--- LeetCode/58.LengthOfLastWord.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} string
- * @return {number}
- */
-var lengthOfLastWord = function (string) {
-    let lastWord = string.trim().split(" ").reverse()[0];
-
-    return lastWord.length;
-};
