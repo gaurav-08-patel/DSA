827d155af76741d32917fe9ac60b7c5e915b3aa0
Gaurav Patel <gauravpatelrounak@gmail.com>
2026-08-18 18:08:50 +0530

    Time: 2 ms (4.4%), Space: 8 MB (96.8%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool strongPasswordCheckerII(string password) {
+        if(password.size() < 8) return false;
+        string special = "!@#$%^&*()-+";
+        bool hasLower=false, hasUpper=false, hasDigit=false, hasSpecial=false;
+
+        for(int i=0; i<password.size(); i++){
+            char c = password[i];
+            if(islower(c)) hasLower = true;
+            else if(isupper(c)) hasUpper=true;
+            else if(isdigit(c)) hasDigit=true;
+            else if(special.find(c) != string::npos) hasSpecial = true;
+
+            if(i > 0 && password[i] == password[i-1]) return false;
+        }
+        
+        return hasLower && hasUpper && hasDigit && hasSpecial;
+    }
+};
