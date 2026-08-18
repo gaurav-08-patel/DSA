e9e2fbb23b3aa172c44e46d253d8f9168bda2da9
Gaurav Patel <gauravpatelrounak@gmail.com>
2026-08-18 17:52:09 +0530

    Time: 4 ms (91.6%), Space: 15.4 MB (98.22%) - LeetHub

--- 3941-password-strength/3941-password-strength.cpp
+++ 3941-password-strength/3941-password-strength.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    int getPoint(char c) {
-        if (c >= 48 && c <= 57) {
-            return 3;
-        } else if (c >= 97 && c <= 122) {
-            return 1;
-        } else if (c >= 65 && c <= 90) {
-            return 2;
-        } else {
-            return 5;
-        }
-    }
+    // int getPoint(char c) {
+    //     if (c >= 48 && c <= 57) {
+    //         return 3;
+    //     } else if (c >= 97 && c <= 122) {
+    //         return 1;
+    //     } else if (c >= 65 && c <= 90) {
+    //         return 2;
+    //     } else {
+    //         return 5;
+    //     }
+    // }
 
     int passwordStrength(string password) {
-        set<char> s(password.begin(), password.end());
-        int strength=0;
 
-        for (auto el : s) {
-            strength = strength + getPoint(el);
+        //****************************not optimal**************
+        // set<char> s(password.begin(), password.end());
+        // int strength=0;
+
+        // for (auto el : s) {
+        //     strength = strength + getPoint(el);
+        // }
+
+        // *************************OPTIMAL solution*************************
+
+        vector<bool> seen(128, false);
+        int strength = 0;
+
+        for (char c : password) {
+            if(!seen[c]){
+                seen[c] = true;
+                if(isdigit(c)) strength += 3;
+                else if(islower(c)) strength += 1;
+                else if(isupper(c)) strength += 2;
+                else strength += 5;
+            }
         }
-
         return strength;
     }
 };
