5760bbbd1ecd605c058ff08d9d972d54e97697e8
Gaurav Patel <gauravpatelrounak@gmail.com>
2026-08-22 00:58:08 +0530

    Time: 0 ms (100%), Space: 10.1 MB (69.52%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,19 +2,36 @@ class Solution {
 public:
     string reverseWords(string s) {
         //*******BETTER appraoch************
-        istringstream iss(s);
-        string word;
-        vector<string> arr;
+        // istringstream iss(s);
+        // string word;
+        // vector<string> arr;
 
-        while(iss >> word) arr.push_back(word);
+        // while(iss >> word) arr.push_back(word);
 
-        string result;
+        // string result;
 
-        for(int i=arr.size()-1; i>=0; i--){
-            result.append(arr[i]);
-            result.append(" ");
-        }
-        result.pop_back();
-        return result;
+        // for(int i=arr.size()-1; i>=0; i--){
+        //     result.append(arr[i]);
+        //     result.append(" ");
+        // }
+        // result.pop_back();
+        // return result;
+
+        //*******************OPTIMAL********************
+        string ans;
+        int i = s.size() - 1;
+
+        while(i >= 0){
+            while(i >= 0 && s[i] == ' ') i--;
+            if(i == -1) break;
+
+            int j=i;
+            while(i >= 0 && s[i] != ' ') i--;
+
+            if (!ans.empty()) ans.push_back(' ');
+            ans.append(s.substr(i+1,j-i));
+        } 
+
+        return ans;
     }
 };
