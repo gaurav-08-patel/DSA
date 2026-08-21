4d85fe891831b7240bc1e4af1460244719df78d5
Gaurav Patel <gauravpatelrounak@gmail.com>
2026-08-21 23:50:29 +0530

    Delete LeetCode/185.DepartmentTopThreeSalaries.sql

--- LeetCode/185.DepartmentTopThreeSalaries.sql
+++
@@ -1,8 +0,0 @@
-SELECT Department , Employee , Salary FROM (
-    SELECT d.name Department ,
-    e.name Employee ,
-    e.salary Salary ,
-    DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS "rnk" 
-    FROM Employee e LEFT JOIN Department d ON e.departmentId = d.id
-) AS Ranked
-WHERE Ranked.rnk <= 3;
