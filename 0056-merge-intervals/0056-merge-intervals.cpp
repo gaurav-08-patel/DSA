afc4bd66c55a71c1ce18e3b6ce5098f98edf35c2
Gaurav Patel <gauravpatelrounak@gmail.com>
2026-08-22 17:29:43 +0530

    Time: 4 ms (66.41%), Space: 23.9 MB (31.67%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        //****************BRUTE/BETTER solution******************
+        // int n = intervals.size();
+        // vector<vector<int>> ans;
+        // sort(intervals.begin(),intervals.end());
+
+        // for(int i=0; i<n; i++){
+        //     int first = intervals[i][0];
+        //     int end = intervals[i][1];
+            
+        //     if(!ans.empty() && ans.back()[1] >= end) continue; // move to next if already in interval
+
+        //     for(int j=i+1; j<n; j++){
+        //         if(intervals[j][0] <= end){
+        //             end = max(end,intervals[j][1]);
+        //         }else break;
+        //     }
+        //     ans.push_back({first,end});
+        // }
+        
+        // return ans;
+
+
+
+        //************OPTIMAl approach***************
         int n = intervals.size();
         vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end());
@@ -8,17 +33,15 @@ public:
         for(int i=0; i<n; i++){
             int first = intervals[i][0];
             int end = intervals[i][1];
-            
-            if(!ans.empty() && ans.back()[1] >= end) continue;
 
-            for(int j=i+1; j<n; j++){
-                if(intervals[j][0] <= end){
-                    end = max(end,intervals[j][1]);
-                }else break;
+            if(ans.empty() || ans.back()[1] < first){ // add new interval if curr inteval and one in interval doesnt overlap
+                ans.push_back({first,end});
+            }else{
+                ans.back()[1] = max(end,ans.back()[1]); // otherwise keep updating its end with curr intervals end but with max
             }
-            ans.push_back({first,end});
+
         }
-        
+
         return ans;
     }
 };
