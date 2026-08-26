08e750784869a000d73968c1b063f108deab3ddd
Gaurav Patel <gauravpatelrounak@gmail.com>
2026-08-27 00:08:57 +0530

    Time: 0 ms (100%), Space: 26.2 MB (70.22%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -11,8 +11,9 @@ public:
         //now we will start from 1 and n-1 index for low and high
         int low = 1, high = n-1;
 
-        // (even , odd) 
-        // (odd , even) 
+        // (even , odd) // if on left side of unique element then repeated elements will have even-odd indices
+        // (odd , even) // if on right side then odd-even
+        // ex : [ 3 , 3 , 7 , 7 , 10 , 11 , 11 ] --> 3-> (0,1) : (even,odd) , 7-> (2,3) : (even,odd) , 11-> (5,6) : (odd,even)
         while(low <= high){
             int mid = (low + high)/2;
             //check if curr mid is unique or not
