fedbf547b14f0b261031f3db03beeff119f880ba
Gaurav Patel <gauravpatelrounak@gmail.com>
2026-08-26 19:27:51 +0530

    Time: 0 ms (100%), Space: 26.1 MB (94.29%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        //concept is to use elimination technique of BS to reduce size of array and keep on checking if Im on unique element or not by checkg its before and after element
+
+        int n = nums.size();
+        // this if's are to eliminate the edge cases and to not write if conditions to check whether Im on first or last index before checking its before and after element to know whether it distinct or not 
+        if(n == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        //now we will start from 1 and n-1 index for low and high
+        int low = 1, high = n-1;
+
+        // (even , odd) 
+        // (odd , even) 
+        while(low <= high){
+            int mid = (low + high)/2;
+            //check if curr mid is unique or not
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+
+            if( (mid % 2 == 1   && nums[mid] == nums[mid-1])  || (mid % 2 == 0 && nums[mid] == nums[mid + 1])  ){
+                low = mid +1;
+            }else {
+                high = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+};
